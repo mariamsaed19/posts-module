00aa4250f83816fd5e6095041d737aa6b97bce38
Mariam Saeed <es-mariamzaho4@alexu.edu.eg>
2021-09-01 13:15:31 +0200

    Edit stylying and add comments

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import axios  from "axios"
 import { useCallback, useEffect, useState } from "react"
 import Card from "./components/Card"
 function App() {
+  //link of api
   const apiUrl="https://jsonplaceholder.typicode.com/posts"
+  //save state to re-render component
   const [posts,setState]=useState([]);
+
+  //get request
  const getPosts = useCallback(()=>{ axios.get(apiUrl)
   .then(res => {
     let fields =[]
     res.data.map( post=> {
+      // save posts in a list
       fields.push(<Card title={post.title} body={post.body} className="card"/>)
     })
+    //re-render component by change its state
     setState(fields)
 
   })
   },[])
 
+  //to make get request once and avoid loops
   useEffect(()=> getPosts() ,[getPosts])
   return (
+   
     <div>
-     {posts}
+      <h1> Posts </h1>
+      <div className="posts"> {posts} </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,13 @@
 import './Card-style.css'
 function Card(props){
+    // add clases from parent component
     const classes = "container "+props.className
     return(
         <div className={classes}>
-            <h2 className="title-holder"> {props.title} </h2>
+            <div className="title-holder">
+                <h3> Title : </h3>
+                <h4>{props.title} </h4>
+            </div>
             <div className="body-holder"> {props.body} </div>
         </div>
     );
